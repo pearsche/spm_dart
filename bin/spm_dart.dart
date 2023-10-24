3298900303653a9971eab9b4c1cef5a722c4e54d
pearsche <thepearsche@proton.me>
2023-10-23 19:32:44 -0500

    initial commit: flake, default dart console application setup

---
+++ bin/spm_dart.dart
@@ -0,0 +1,5 @@
+import 'package:spm_dart/spm_dart.dart' as spm_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${spm_dart.calculate()}!');
+}
---
+++ test/spm_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:spm_dart/spm_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
