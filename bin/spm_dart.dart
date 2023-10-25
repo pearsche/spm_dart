d21536c0c7c47be3c0deaf8afad47a5892851413
pearsche <thepearsche@proton.me>
2023-10-24 21:38:11 -0500

    add english dictionary copied from the rust spm, create actual passphrase generator function.

--- bin/spm_dart.dart
+++ bin/spm_dart.dart
@@ -1,5 +1,9 @@
 import 'package:spm_dart/spm_dart.dart' as spm_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${spm_dart.calculate()}!');
+void main(List<String> arguments) async {
+  //print('Hello world: ${spm_dart.calculate()}!');
+  var randomWord = await spm_dart.getRandomWord();
+  print(randomWord);
+
+  print(await spm_dart.constructPassphrase(3));
 }
--- lib/spm_dart.dart
+++ lib/spm_dart.dart
@@ -1,3 +1,41 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:math';
+
 int calculate() {
   return 6 * 7;
+}
+
+Stream<String> loadDictionary() {
+  var dictionaryFilename = File('english-dictionary.txt');
+  Stream<List<int>> streamedDictionary = dictionaryFilename.openRead();
+  var streamedDictionaryContents =
+      utf8.decoder.bind(streamedDictionary).transform(const LineSplitter());
+  return streamedDictionaryContents;
+}
+
+Future getDictionarySize() {
+  var streamedDictionaryContents = loadDictionary();
+  return streamedDictionaryContents.length;
+}
+
+Future<String> getRandomWord() async {
+  var streamedDictionaryContents = loadDictionary();
+  var random = Random();
+  // Get a random number, from 0 to the dictionary size
+  var indexOfWordToGet = random.nextInt(await getDictionarySize());
+  return streamedDictionaryContents.elementAt(indexOfWordToGet);
+}
+
+Future<String> constructPassphrase(int length) async {
+  final passphraseBuffer = StringBuffer('');
+  while (length > 0) {
+    length -= 1;
+    passphraseBuffer.write(await getRandomWord());
+    //separator
+    if (length >= 1) {
+      passphraseBuffer.write("-");
+    }
+  }
+  return passphraseBuffer.toString();
 }
